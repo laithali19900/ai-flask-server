f1bb012e3dbbd8bbc704794738859798bb2507cc
Laith <laithali1990@gmail.com>
2025-04-06 22:15:54 +0300

    fix: correct Procfile and startup config

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-from flask import Flask, request, send_file
-from flask_cors import CORS
-from PIL import Image, ImageDraw
-import io
-
-from flask import flask
+from flask import Flask
 app = Flask(__name__)
+
 CORS(app)
 
 @app.route('/process', methods=['POST'])
